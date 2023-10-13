7f2cd993bbebcce097f78cd15fba5583c713d46c
Ernest_Benedict_Othieno <ernestben69@gmail.com>
2023-10-13 14:30:15 +0300

    Initial commit

---
+++ demo/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+import com.example.emp.domain.Student;
+
+
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repo;
+
+    public List<Student> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Student std) {
+        repo.save(std);
+    }
+
+    public Student get(long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+
+}
